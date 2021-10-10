304bc60deaf3f5e8fe3121792ea26796e6d020cc
JongeunKeum <jon9eun@gmail.com>
2021-10-10 12:19:31 +0900

    add board page

--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from board.views import main
+from board.views import main, board
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name="main"),
+    path('board/', board, name="board"),
 ]
