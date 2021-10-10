96845f605e2f697f6948b815b8ff15f339fe05a2
JongeunKeum <jon9eun@gmail.com>
2021-10-10 11:57:21 +0900

    add main page

--- board/views.py
+++ board/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import requests
 
 
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+def main(request):
+    return render(request, 'main.html')
