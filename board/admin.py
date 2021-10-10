2787eb36b0b66179d899a0a321fa93607655d751
JongeunKeum <jon9eun@gmail.com>
2021-10-10 09:52:54 +0900

    add admin

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'view_count',
+        'tag',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'title',
+        'content',
+    )
