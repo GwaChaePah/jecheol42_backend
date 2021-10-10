04c5edb0091fb780f234fc16a74784128b53bd00
JongeunKeum <jon9eun@gmail.com>
2021-10-10 11:57:21 +0900

    add main page

--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from board.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name=main),
 ]
