c359f6a981db515900679cecb8f2e6909d0bf8a1
JongeunKeum <jon9eun@gmail.com>
2021-10-10 12:24:46 +0900

    modify DATABASES

--- gwachaepah_practice/settings.py
+++ gwachaepah_practice/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'gwachaepah_practice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'gwachaepah_practice',
-        'NAME': 'd2e9380q2gij4t',
-        # 'USER': 'root',
-        'USER': 'qvhpclmqzpdqlm',
-        # 'PASSWORD': 'password',
-        'PASSWORD': 'fc1b4bab6982e9270816f991b2c9037c3795f8238a48a18a6aba4cbc9ee20e18',
-        # 'HOST': 'localhost',
-        'HOST': 'ec2-44-199-86-61.compute-1.amazonaws.com',
+        'NAME': 'gwachaepah_practice',
+        # 'NAME': 'd2e9380q2gij4t',
+        'USER': 'root',
+        # 'USER': 'qvhpclmqzpdqlm',
+        'PASSWORD': 'password',
+        # 'PASSWORD': 'fc1b4bab6982e9270816f991b2c9037c3795f8238a48a18a6aba4cbc9ee20e18',
+        'HOST': ALLOWED_HOSTS,
+        # 'HOST': 'ec2-44-199-86-61.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
