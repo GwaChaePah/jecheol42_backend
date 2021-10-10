ea1676014f19cdb3937697f2f9d06b888063a312
JongeunKeum <jon9eun@gmail.com>
2021-10-10 10:41:29 +0900

    add index

--- board/views.py
+++ board/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
