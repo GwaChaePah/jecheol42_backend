e94bb30c62f504eef90d2d15c6373726be186948
JongeunKeum <jon9eun@gmail.com>
2021-10-10 14:19:00 +0900

    add media

--- gwachaepah_practice/settings.py
+++ gwachaepah_practice/settings.py
@@ -134,6 +134,9 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from board.views import main, board
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name="main"),
     path('board/', board, name="board"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
