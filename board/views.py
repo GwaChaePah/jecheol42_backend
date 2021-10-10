5e87c5fd23055d8c02de22d93fd58ea5c2a96f21
JongeunKeum <jon9eun@gmail.com>
2021-10-10 15:44:03 +0900

    Fix: show main post

--- board/views.py
+++ board/views.py
@@ -3,7 +3,10 @@ from .models import Post
 
 
 def main(request):
-    return render(request, 'main.html')
+    context = {
+        'posts': Post.objects.all().order_by('-created_at')
+    }
+    return render(request, 'main.html', context)
 
 
 def board(request):
