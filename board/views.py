36ae76597113ee42ccf17339829e6c861927e67d
JongeunKeum <jon9eun@gmail.com>
2021-10-10 12:19:31 +0900

    add board page

--- board/views.py
+++ board/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def main(request):
     return render(request, 'main.html')
+
+
+def board(request):
+    context = {
+        'posts': Post.objects.all().order_by('-created_at')
+    }
+    return render(request, 'board.html', context)
