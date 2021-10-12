b4b898fb0394197a6edc995a39705a38387856d3
3Cat-jaguar <ryusa13@gmail.com>
2021-10-12 16:05:19 +0900

    Feat: modify views.py to read product data in main.html

--- board/views.py
+++ board/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Product, Comment
 
 
 def main(request):
     context = {
-        'posts': Post.objects.all().order_by('-created_at')
+        'product': Product.objects.all()
     }
     return render(request, 'main.html', context)
 
 
 def board(request):
     context = {
-        'posts': Post.objects.all().order_by('-created_at')
+        'posts': Post.objects.all().order_by('-created_at'),
+        'comments': Comment.objects.all().order_by('-created_at'),
     }
     return render(request, 'board.html', context)
+
