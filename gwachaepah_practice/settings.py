5068160d89f7bd4592f3cc0c8650dd282b71c257
3Cat-jaguar <ryusa13@gmail.com>
2021-10-12 11:04:47 +0900

    Fix: import undefined file error

--- gwachaepah_practice/settings.py
+++ gwachaepah_practice/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
-import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
