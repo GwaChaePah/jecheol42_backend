b9ac57c5ad7eabf0d32d80923bb2e96930597839
3Cat-jaguar <ryusa13@gmail.com>
2021-10-15 12:42:05 +0900

    Fix: image upload error

--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name="main"),
     path('board/', board, name="board"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
