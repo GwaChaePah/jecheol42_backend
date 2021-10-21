22bcf41ea6110e7b2bb285eb0c7a6df9126073ff
JongeunKeum <jon9eun@gmail.com>
2021-10-21 15:44:42 +0900

    Feat: request open api json data & make search page

--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from board.views import main, board
+from board.views import main, board, search
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name="main"),
     path('board/', board, name="board"),
+    path('search/', search, name="search"),
 ]
 
 
