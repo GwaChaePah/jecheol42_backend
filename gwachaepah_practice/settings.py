8cda153d4694fe7d70208544bdfba5d200d4b950
stopminbeom <mseo@student.42seoul.kr>
2021-10-25 15:06:02 +0900

    style: delete media folder and popup main 2 to 4

--- gwachaepah_practice/settings.py
+++ gwachaepah_practice/settings.py
@@ -125,9 +125,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
