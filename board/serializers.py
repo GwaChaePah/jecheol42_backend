2f40847a6bd84ee2c226be1325cdeab73865ef43
stopminbeom <mseo@student.42seoul.kr>
2021-11-03 15:52:41 +0900

    Feat: add post-api

--- board/serializers.py
+++ board/serializers.py
@@ -1,4 +1,4 @@
-from .models import Product
+from .models import Product, Post
 from rest_framework import serializers
 
 
@@ -6,3 +6,22 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'image', 'month']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'content',
+            'image1',
+            'image2',
+            'image3',
+            'view_count',
+            'created_at',
+            'updated_at',
+            'tag',
+            'price',
+            'user_key'
+        ]
