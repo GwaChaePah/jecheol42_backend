20f8b446f095c6a67ae5394484195db23d9e2ac4
JongeunKeum <jon9eun@gmail.com>
2021-11-03 20:36:34 +0900

    Feat: add search api

--- gwachaepah_practice/urls.py
+++ gwachaepah_practice/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from board.views import main, board, search, ProductList, PostList, CommentList
+from board.views import main, board, search, ProductList, PostList, CommentList, SearchList
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -27,6 +27,7 @@ urlpatterns = [
     path('product-api/', ProductList.as_view()),
     path('post-api/', PostList.as_view()),
     path('comment-api/<int:pk>/', CommentList.as_view()),
+    path('search-api', SearchList.as_view()),
 ]
 
 
