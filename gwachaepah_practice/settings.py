1d09ca99398228be7aa60396b6454c5af04936b8
stopminbeom <mseo@student.42seoul.kr>
2021-11-07 14:11:37 +0900

    Modify: api page size change

--- gwachaepah_practice/settings.py
+++ gwachaepah_practice/settings.py
@@ -149,5 +149,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 12
 }
